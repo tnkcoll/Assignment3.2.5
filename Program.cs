ab72fc363c492b39a57840c0b64e80d5e8e04c03
Ted Collins <theodore.collins2023@outlook.com>
2024-05-21 15:41:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment3._2._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 5, 3 };
+            int target = 5;
+
+            PrintItems(nums, target);
+            
+        }
+
+        public static int GetIndex(int[] nums, int target) 
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target) return i;
+            }
+            return -1;
+        }
+
+        public static void PrintItems(int[] nums, int target)
+        {
+            for(int i = 0;i < nums.Length;i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The target value to find is {target}");
+            Console.WriteLine($"The index is {GetIndex(nums, target)}");
+        }
+    }
+}
